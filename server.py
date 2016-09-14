bffcd763e5191ebd4be9de4e15a6e15c14c08d6d
ColleenRothe <colleenrothe516@gmail.com>
2016-09-13 18:25:45 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,33 @@
 #server.py
 #import bottle
-from bottle import route, run, template
+from bottle import route, run, template, static_file
 
 ##Just playing with this stuff to show the board...
 ##not actually sure where it should go
 ##http://bottlepy.org/docs/dev/tutorial.html#quickstart-hello-world
 
+#opponent board
+
+#own board
+global gameBoard
+def print_board(gameBoard):
+	for r in gameBoard:	
+		for x in r:
+			print(x)
+
+
+with open ('board.txt') as file:
+	gameBoard = [[n for n in line.split()] for line in file]
+	print_board(gameBoard)
+
+
 ##goes to either
 @route('/<board>')
 def go(board):
     if(board == 'own_board.html'):
-        return "Hello Own"
+        #return "Hello Own"
+        #return gameBoard
+        return static_file('board.txt', root='')
     elif(board == 'opponent_board.html'):
         return "Hello Opponent"
 run(host='localhost', port=5000)
