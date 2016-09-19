29817b4704c29686528213324e9e647b52732dbf
ColleenRothe <colleenrothe516@gmail.com>
2016-09-18 18:25:53 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,38 +1,138 @@
-#server.py
-#import bottle
-from bottle import route, run, template, static_file
+from bottle import get, run, request, response, route, template, Bottle
+import sys
+import http.client
+import urllib
+from urllib import parse
 
-##Just playing with this stuff to show the board...
-##not actually sure where it should go
-##http://bottlepy.org/docs/dev/tutorial.html#quickstart-hello-world
+#test integer:
+#http://stackoverflow.com/questions/5424716/python-how-to-check-if-input-is-a-number
 
-#opponent board
+#how to do the table
+#http://www.w3schools.com/tags/tag_tr.asp
 
-#own board
-global gameBoard
-def print_board(gameBoard):
-	for r in gameBoard:	
-		for x in r:
-			print(x)
-
-
-with open ('board.txt') as file:
-	gameBoard = [[n for n in line.split()] for line in file]
-	print_board(gameBoard)
-
-
-##goes to either
-@route('/<board>')
-def go(board):
-    if(board == 'own_board.html'):
-        #return "Hello Own"
-        #return gameBoard
-        return static_file('board.txt', root='')
-    elif(board == 'opponent_board.html'):
-        return "Hello Opponent"
-run(host='localhost', port=5000)
+#accept port and file for the board
+port = sys.argv[1]
+file = sys.argv[2]
 
 
 
 
+#create own board
+ownboard = []
+board = open(file)
+for i in range(0,10):
+    row = board.readline().strip('\n') #newline creating probs
+    ownboard.append([]) #2D
+    for thing in row:
+        ownboard[i].append(thing)
 
+#create opponent board
+opponentboard = [['_' for n in range(0,10)] for n in range (0, 10)]
+
+#start bottle
+app= Bottle()
+
+#need to deal with:
+#out of bounds: HTTP Not Found
+#already fired upon: HTTP Gone
+#bad format: HTTP Bad Request (not an int?)
+@app.post('/')
+def process_action():
+    x=request.POST['x']
+    y=request.POST['y']
+
+    #for the sinks..where to put? not here!
+    cnum = 0
+    bnum = 0
+    rnum = 0
+    snum = 0
+    dnum = 0
+   
+
+    try: #have to do first so it can be int for the rest
+        x=int(x)
+        y=int(y)
+    except ValueError:
+        print("GOT UP HERE")
+        response.status = 400 #bad request
+        return
+    if x == '10' or y>=10 or x<0 or y<0:
+        print("GOT HERE")
+        response.status = 404 #out of bounds
+        return
+    #look at board
+    if ownboard[x][y] == "_":
+        #miss
+        ownboard[x][y] = "O" #nothing there
+        return parse.urlencode({'hit': 0})
+    elif ownboard[x][y] == "O" or ownboard[x][y] == "X":
+        #already hit there 410 gone
+        response.status = 410
+        return
+    else:
+        #hit something....
+        ownboard[x][y] = "X"
+
+        if ownboard[x][y] == "C":
+            cnum+=1
+        elif ownboard[x][y] == "B":
+            bnum+=1
+        elif ownboard[x][y] == "R":
+            rnum +=1
+        elif ownboard[x][y] == "S":
+            snum +=1
+        elif ownboard[x][y] == "D":
+            dnum +=1
+
+        if cnum == 5:
+            return parse.urlencode({'hit':1, 'sink':'C'})
+        elif bnum == 4:
+             return parse.urlencode({'hit':1, 'sink':'B'})
+        elif rnum == 3:
+             return parse.urlencode({'hit':1, 'sink':'R'})
+        elif snum == 3:
+             return parse.urlencode({'hit':1, 'sink':'S'})
+        elif dnum == 2:
+             return parse.urlencode({'hit':1, 'sink':'D'})
+        else:
+            return parse.urlencode({'hit':1})
+
+            
+#doesn't exactly work
+@app.post('/sendback')
+def do_opponent():
+    x1=request.POST['x']
+    y1=request.POST['y']
+    hit=request.POST['hit']
+    print("X IS")
+    print(x1)
+    #do more here...copy same idea from above to update opponent board
+    
+
+
+#show own board
+@app.get('/own_board.html')
+def own_board(): #<tr> = row containing 1+ <td>elements
+    board = "Your Own Board <table>" #start table
+    for i in range(0,10):
+        board +=str("<tr>") #new row
+        for j in range(0,10):
+            board +=str("<td>") + str(ownboard[i][j]) + str("<td>") #new thing
+        board +=str("</tr>") #new row
+    board += str("<table>") #end table
+    return board
+
+#show opponent board
+@app.get('/opponent_board.html')
+def opponent_board(): #<tr> = row containing 1+ <td>elements
+    board = "Opponent Board <table>" #start table
+    for i in range(0,10):
+        board +=str("<tr>") #new row
+        for j in range(0,10):
+            board +=str("<td>") + str(opponentboard[i][j]) + str("<td>") #new thing
+        board +=str("</tr>") #new row
+    board += str("<table>") #end table
+    return board
+
+
+run(app, host='0.0.0.0', port=port) #only one run for all
